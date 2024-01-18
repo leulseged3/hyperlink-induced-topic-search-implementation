79c036f5a5317eb810b766199123af7c8b3ca74b
Leulseged <leulseged3@gmail.com>
2024-01-18 14:44:47 +0200

    👔 update the table creation & topic insertion logic

--- src/create-tables.py
+++ src/create-tables.py
@@ -1,20 +1,28 @@
+from psycopg2 import sql
 from utils import db_connect, queries
-
+    
+def table_exists(cursor, table_name):
+    query = sql.SQL("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = {});").format(
+        sql.Literal(table_name)
+    )
+    cursor.execute(query)
+    return cursor.fetchone()[0]
 
 # create tables for the first dataset "news-aggregate-dataset-1"
 connection = db_connect.create_connection("news-aggregate-dataset-1")
 
 if connection:
     cursor = connection.cursor()
-    cursor.execute(queries.create_topics_table_query)
+    if not table_exists(cursor, "topics"):
+        cursor.execute(queries.create_topics_table_query)
 
-    cursor.execute(queries.insert_topics_query)
-    connection.commit()
-
-    cursor.execute(queries.create_news_table_query)
-    connection.commit()
-    cursor.close()
-    connection.close()
+        cursor.execute(queries.insert_first_topics_query)
+        connection.commit()
+    if not table_exists(cursor, "news"):
+        cursor.execute(queries.create_news_table_query)
+        connection.commit()
+        cursor.close()
+        connection.close()
 else:
     print("Error: Database connection not established.")
 
@@ -24,14 +32,15 @@ connection = db_connect.create_connection("news-aggregate-dataset-2")
 
 if connection:
     cursor = connection.cursor()
-    cursor.execute(queries.create_topics_table_query)
+    if not table_exists(cursor, "topics"):
+        cursor.execute(queries.create_topics_table_query)
 
-    cursor.execute(queries.insert_topics_query)
-    connection.commit()
-
-    cursor.execute(queries.create_news_table_query)
-    connection.commit()
-    cursor.close()
-    connection.close()
+        cursor.execute(queries.insert_second_topics_query)
+        connection.commit()
+    if not table_exists(cursor, "news"):
+        cursor.execute(queries.create_news_table_query)
+        connection.commit()
+        cursor.close()
+        connection.close()
 else:
     print("Error: Database connection not established.")
